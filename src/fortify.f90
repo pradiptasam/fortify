60031eb06d88ac0632e11a42dd83a6aa8c3dbbe5
Pradipta Samanta <samanta@dkrz.de>
2025-01-06 21:28:55 +0100

    refactor: added a wrapper module for the fortify framework

---
+++ src/fortify.f90
@@ -0,0 +1,9 @@
+module Fortify
+    ! Wrapper module for the Fortify framework
+    use check_assertions
+    use fortify_runner
+    implicit none
+
+    ! Re-export all public procedures from used modules
+    public
+end module Fortify
